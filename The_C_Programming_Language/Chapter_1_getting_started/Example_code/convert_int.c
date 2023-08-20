6292e81e75f6bf3c77aaa512bdba1ae2e9bcb5b4
Alpaca man <johnerhover@gmail.com>
2023-08-20 19:41:03 +0900

    converter program with float added

--- The_C_Programming_Language/Chapter_1_getting_started/Example_code/convert_int.c
+++ The_C_Programming_Language/Chapter_1_getting_started/Example_code/convert_int.c
@@ -18,4 +18,29 @@ main()
         printf("%d\t%d\n",fahr,cels); // %d\t correspond to fahr. %d\n correspond to cels.
         fahr=fahr+step;
     }
+
+     int convert_int_with_digits();
+}
+
+int convert_int_with_digits()
+{
+    printf("\n");
+    int fahr, cels;
+    int lower, upper, step;
+
+    lower =0;
+    upper =300;
+    step=20;
+
+    fahr = lower;
+
+    printf("F\tC\t\n");
+    while (fahr<=upper)
+    {
+        cels= 5*(fahr-32)/9;
+        printf("%3d\t %6d\n",fahr,cels);
+        fahr = fahr+step;
+    }
+
+    return 0;
 }
